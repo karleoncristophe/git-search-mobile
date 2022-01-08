d921f190c9bd9bd8305b4c8f76b93bda909e45e2
Karleon Cristophe <karleoncristophe@hotmail.com>
2022-01-08 16:11:16 -0300

    loading added

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
-import {ScrollView, View} from 'react-native';
+import {ActivityIndicator, ScrollView, View} from 'react-native';
 import styled from 'styled-components/native';
 
 const Container = styled.View<any>`
@@ -98,92 +98,124 @@ interface User {
 
 const Profile: React.FC = ({navigation, route}: any) => {
   const [user, setUser] = useState({} as User);
+  const [loading, setLoading] = useState(false);
   const baseURL: string = `https://api.github.com/users/${route.params.name}`;
   useEffect(() => {
     const getUser = async () => {
-      console.log(`${route.params.name} here`);
+      if (loading) {
+        return;
+      }
+      setLoading(true);
+
       const {data} = await axios.get(`${baseURL}`);
       setUser(data);
+      setLoading(false);
     };
     getUser();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
   return (
-    <Container>
-      <Navigation onPress={() => navigation.goBack()}>
-        <Image source={require('../assets/icons/arrow-left.png')} />
-        <Text>Go Back</Text>
-      </Navigation>
+    <>
+      {loading === false ? (
+        <Container>
+          <Navigation onPress={() => navigation.goBack()}>
+            <Image source={require('../assets/icons/arrow-left.png')} />
+            <Text>Go Back</Text>
+          </Navigation>
 
-      <ScrollView>
-        <Content>
-          <AvatarAndNameContent>
-            <Avatar
-              source={{
-                uri: `${user?.avatar_url}`,
-              }}
-            />
+          <ScrollView>
+            <Content>
+              <AvatarAndNameContent>
+                <Avatar
+                  source={{
+                    uri: `${user?.avatar_url}`,
+                  }}
+                />
 
-            <TitleName>{user?.name ? user?.name : 'No Name'}</TitleName>
-          </AvatarAndNameContent>
+                <TitleName>{user?.name ? user?.name : 'No Name'}</TitleName>
+              </AvatarAndNameContent>
 
-          <FollowsContent>
-            <View>
-              <Span>Followers</Span>
-              <Text style={{textAlign: 'center'}}>{user?.followers}</Text>
-            </View>
-            <View>
-              <Span>Following</Span>
-              <Text style={{textAlign: 'center'}}>{user?.following}</Text>
-            </View>
-          </FollowsContent>
-          <InformationsContent>
-            <Span>Bio</Span>
-            <Text>{user?.bio ? user?.bio : 'No Bio'}</Text>
-          </InformationsContent>
-          <InformationsContent>
-            <Span>Twitter</Span>
-            <Text
-              style={{
-                textTransform: user?.twitter_username
-                  ? 'lowercase'
-                  : 'capitalize',
-              }}>
-              {user?.twitter_username
-                ? `@${user?.twitter_username}`
-                : 'No Twitter'}
-            </Text>
-          </InformationsContent>
+              <FollowsContent>
+                <View>
+                  <Span>Followers</Span>
+                  <Text style={{textAlign: 'center'}}>{user?.followers}</Text>
+                </View>
+                <View>
+                  <Span>Following</Span>
+                  <Text style={{textAlign: 'center'}}>{user?.following}</Text>
+                </View>
+              </FollowsContent>
+              <InformationsContent>
+                <Span>Bio</Span>
+                <Text>{user?.bio ? user?.bio : 'No Bio'}</Text>
+              </InformationsContent>
+              <InformationsContent>
+                <Span>Twitter</Span>
+                <Text
+                  style={{
+                    textTransform: user?.twitter_username
+                      ? 'lowercase'
+                      : 'capitalize',
+                  }}>
+                  {user?.twitter_username
+                    ? `@${user?.twitter_username}`
+                    : 'No Twitter'}
+                </Text>
+              </InformationsContent>
 
-          <InformationsContent>
-            <Span>Location</Span>
-            <Text>{user?.location ? user?.location : 'No Location'}</Text>
-          </InformationsContent>
+              <InformationsContent>
+                <Span>Location</Span>
+                <Text>{user?.location ? user?.location : 'No Location'}</Text>
+              </InformationsContent>
 
-          <InformationsContent>
-            <Span>Email</Span>
-            <Text>{user?.email ? user?.email : 'No Email'}</Text>
-          </InformationsContent>
-          <InformationsContent>
-            <Span>Company</Span>
-            <Text>{user?.company ? user?.company : 'No Company'}</Text>
-          </InformationsContent>
+              <InformationsContent>
+                <Span>Email</Span>
+                <Text>{user?.email ? user?.email : 'No Email'}</Text>
+              </InformationsContent>
+              <InformationsContent>
+                <Span>Company</Span>
+                <Text>{user?.company ? user?.company : 'No Company'}</Text>
+              </InformationsContent>
 
-          <InformationsContent>
-            <Span>Public Gists</Span>
-            <Text> {user?.public_gists ? user?.public_gists : 0}</Text>
-          </InformationsContent>
-          <InformationsContent>
-            <Span>Public Repos</Span>
-            <Text> {user.public_repos ? user?.public_repos : 0}</Text>
-          </InformationsContent>
-          <InformationsContent>
-            <Span>Id</Span>
-            <Text>{user?.id ? user?.id : 'No Id'}</Text>
-          </InformationsContent>
-        </Content>
-      </ScrollView>
-    </Container>
+              <InformationsContent>
+                <Span>Public Gists</Span>
+                <Text> {user?.public_gists ? user?.public_gists : 0}</Text>
+              </InformationsContent>
+              <InformationsContent>
+                <Span>Public Repos</Span>
+                <Text> {user.public_repos ? user?.public_repos : 0}</Text>
+              </InformationsContent>
+              <InformationsContent>
+                <Span>Id</Span>
+                <Text>{user?.id ? user?.id : 'No Id'}</Text>
+              </InformationsContent>
+            </Content>
+          </ScrollView>
+        </Container>
+      ) : (
+        <Load load={loading} />
+      )}
+    </>
   );
 };
+
+const LoadContet = styled.View`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  width: 100%;
+  background: #101111;
+`;
+
+interface Props {
+  load: boolean;
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function Load({load}: Props) {
+  return (
+    <LoadContet>
+      <ActivityIndicator size={100} color="#52b788" />
+    </LoadContet>
+  );
+}
 
 export default Profile;
